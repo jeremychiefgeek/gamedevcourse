eefebd41e295ce0daca793b6e5eb2dce1fa4c9fc
Jeremy Evans <jeremy@chiefgeek.io>
2025-11-20 14:36:28 -0500

    updating

---
+++ phase/one/exercises/AdditionQuiz.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() 
+{
+  std::cout << "Enter an integer: ";
+
+  int a{};
+  std::cin >> a;
+
+  std::cout << "Enter another integer: ";
+
+  int b{};
+  std::cin >> b;
+
+  std::cout << a << " + " << b << " = " << a + b << '\n';
+  std::cout << a << " - " << b << " = " << a - b << '\n';
+}
+
